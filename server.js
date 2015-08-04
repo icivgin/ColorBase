262b7521d2788e7c8b3ce0dc4f47090d07bfce9e
Ian Civgin <imciv2@gmail.com>
2015-08-04 01:46:48 -0700

    please work

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/search/:kuler', function (req, res) {
 app.get('/search', function (req, res) {
 	phantom.create(function (ph) {
 	  ph.createPage(function (page) {
-	    page.open('https://color.adobe.com/sandy-stone-beach-ocean-diver-color-theme-15325/edit/?copy=true', function (status) {
+	    page.open('https://color.adobe.com/create/color-wheel/?copy=true&base=0&rule=Analogous&selected=3&name=Copy%20of%20Firenze&mode=rgb&rgbvalues=0.27451,0.537255,0.4,0.2706947088417046,0.5872550000000001,0.5210540687390179,0.28913267591506825,0.627255,0.5035843169878103,0.28913267591506825,0.49411764705882355,0.35492874572482586,0.2706947088417046,0.5872550000000001,0.322720939059917&swatchOrder=0,1,2,3,4', function (status) {
 	    	page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
 		      page.evaluate(function () { return $('.themeBox').html(); }, function (result) {
 		        var re = new RegExp(/#[A-Z0-9]{6}/g);
@@ -46,8 +46,8 @@ app.get('/search', function (req, res) {
 		        console.log(hexCodes);
 		        res.send(hexCodes);
 		        ph.exit();
-		    });
-	      });
+		      });
+	      	});
 	    });
 	  });
 	});
