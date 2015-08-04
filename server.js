eb155d0399d1dbbe7d0c9a3eeff094e292f29339
Ian Civgin <imciv2@gmail.com>
2015-08-03 23:48:27 -0700

    added heroku port to server

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.get('/search/:kuler', function (req, res) {
 	});
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Server started on localhost:3000');
 });
