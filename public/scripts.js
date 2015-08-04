4e86dad0c3e42dd089c50d10283d6e2d6df3352f
Ian Civgin <imciv2@gmail.com>
2015-08-04 12:02:00 -0700

    it almost works like this. gonna change directions

--- public/scripts.js
+++ public/scripts.js
@@ -5,23 +5,26 @@ function replaceString (inputString) {
 	return inputString.replace(re, '%2f');
 }
 
-// $('#import-kuler').on('submit', function (e) {
-// 	e.preventDefault();
-// 	console.log(encodeURIComponent($('#kuler-url').val()));
-// 	$.get('/search/' + encodeURIComponent($('#kuler-url').val()), function (data) {
-// 		$('#examples').show();
-// 		$('body').css('background-color', data[4]);
-// 		$('.change').css('color', data[2]);
-// 	})
-// });
-
 $('#import-kuler').on('submit', function (e) {
 	e.preventDefault();
 	console.log(encodeURIComponent($('#kuler-url').val()));
-	$.get('/search', function (data) {
-		console.log(data);
+	$.get('/search/' + encodeURIComponent($('#kuler-url').val()), function (data) {
+		$('#examples').show();
+		$('body').css('background-color', data[4]);
+		$('.change').css('color', data[2]);
 	})
 });
+
+// $('#import-kuler').on('submit', function (e) {
+// 	e.preventDefault();
+// 	console.log(encodeURIComponent($('#kuler-url').val()));
+// 	$.get('/search', function (data) {
+// 		console.log(data);
+// 		$('#examples').show();
+//  		$('body').css('background-color', data[4]);
+//  		$('.change').css('color', data[2]);
+// 	})
+// });
 
 
 $('#reset-color').on('click', function (e) {
